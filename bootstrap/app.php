c34ed9447e982ece499c16b565409d70b923d218
Isaac11-D <utp0155776@alumno.utpuebla.edu.mx>
2025-08-03 20:11:51 -0600

    Agregar middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->append(\App\Http\Middleware\ForceHttps::class); // Agrega el middleware global
         // Tu configuración de middleware actual
     })
     ->withExceptions(function (Exceptions $exceptions) {
